acc0eaa6e939c76f9d9a03a007b56af211074138
Israel Vargas <Israel.vargas02@inacapmail.cl>
2026-04-13 15:17:28 -0400

    Se crearon los sigueintes archivos Html, Css, JavaScript files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Script de ejemplo para la Evaluación 1
+console.log("Página cargada correctamente.");
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert("¡Bienvenido a la Evaluación 1!");
+});
