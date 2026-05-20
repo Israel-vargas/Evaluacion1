2a700b25561947d2cde9c893f7f72d71e4427d43
Israel Vargas <Israel.vargas02@inacapmail.cl>
2026-05-19 21:35:24 -0400

    Arrow fuction para render dinamico con forEach

--- js/main.js
+++ js/main.js
@@ -5,3 +5,31 @@ const maestros = [
 	{ id: 4, nombre: "Juan Pérez", ciudad: "Frutillar", edad: 30, habilidad: "Pintor", precioServicio: 35000, esFavorito: false },
 	{ id: 5, nombre: "Laura Martínez", ciudad: "Osorno", edad: 25, habilidad: "Electricista", precioServicio: 40000, esFavorito: false }
 ];
+
+const renderizarMaestros = (arreglo) => {
+	const contenedor = document.querySelector('#contenedor-usuarios');
+	if (!contenedor) return;
+	contenedor.innerHTML = '';
+
+	arreglo.forEach((maestro) => {
+		const precioFormateado = maestro.precioServicio.toLocaleString('es-CL', {
+			style: 'currency',
+			currency: 'CLP',
+		});
+
+		contenedor.innerHTML += `
+			<div class="usuario-card">
+				<h3>${maestro.nombre}</h3>
+				<p>Ciudad: ${maestro.ciudad}</p>
+				<p>Edad: ${maestro.edad}</p>
+				<p>Habilidad: ${maestro.habilidad}</p>
+				<p>Precio: ${precioFormateado}</p>
+				<button class="btn-favorito">Agregar a Favoritos</button>
+			</div>
+		`;
+	});
+};
+
+renderizarMaestros(maestros);
+
+
