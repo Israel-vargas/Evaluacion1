8343948c88ddcb8b0b0015f37f2f53cb21d5b4ad
Israel Vargas <Israel.vargas02@inacapmail.cl>
2026-05-19 21:29:24 -0400

    Preparacion HTML y creacion de arreglo de maestros

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const maestros = [
+	{ id: 1, nombre: "Ana García", ciudad: "Puerto Varas", edad: 28, habilidad: "Mecánico", precioServicio: 45000, esFavorito: false },
+	{ id: 2, nombre: "Carlos López", ciudad: "Puerto Montt", edad: 35, habilidad: "Constructor", precioServicio: 48000, esFavorito: false },
+	{ id: 3, nombre: "María Rodríguez", ciudad: "Llanquihue", edad: 42, habilidad: "Gasfitería", precioServicio: 42000, esFavorito: false },
+	{ id: 4, nombre: "Juan Pérez", ciudad: "Frutillar", edad: 30, habilidad: "Pintor", precioServicio: 35000, esFavorito: false },
+	{ id: 5, nombre: "Laura Martínez", ciudad: "Osorno", edad: 25, habilidad: "Electricista", precioServicio: 40000, esFavorito: false }
+];
